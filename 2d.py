994c06015b7a386fe8a09a5710790e23f38f01a5
Dawid <dawid.badura@outlook.com>
2021-10-19 14:49:10 +0200

    Add file with task d

---
+++ 2d.py
@@ -0,0 +1,8 @@
+# Utwórz pętlę iterującą po liście zawierającej 10 liczb (rekomendowane
+# wykorzystanie funkcji range ), która wyświetla co drugi element.
+
+def everySecoundNumber():
+    for i in range(0,10,2):
+        print(i)
+        
+everySecoundNumber()
