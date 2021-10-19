8a8d61592471d39f2bcd2300a7d1ae6983174864
Dawid <dawid.badura@outlook.com>
2021-10-19 14:24:39 +0200

    Add file with task a

---
+++ 2a.py
@@ -0,0 +1,9 @@
+# Utwórz funkcję, która otrzyma w parametrze listę 5 imion, a następnie
+# wyświetli każde z nich.
+
+def printNames(names):
+    for name in names:
+        print(name)
+
+names = ['Dawid', 'Asia', 'Katarzyna', 'Marek', 'Bartek']
+printNames(names)
